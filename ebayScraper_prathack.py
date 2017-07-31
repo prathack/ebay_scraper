15248fd3b8168dc892d30196ed737c1cc287badc
Durga Prathap <prathap.alltechtuts@gmail.com>
2017-07-30 22:21:12 -0700

    Update ebayScraper_prathack.py

--- ebayScraper_prathack.py
+++ ebayScraper_prathack.py
@@ -13,19 +13,23 @@ def totalPages(pages):
 def startCrawl():
     search_string = enterProduct(name)
     r = totalPages(pages)
-    for i in range(1,r+1):
-        print("Titles and prices are scraping for the followning search term: "+search_string+" on page "+str(i))
-        try:
-            url="http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw="+str(search_string)+"&_pgn="+str(i)+"&_skc=100&rt=nc"
-            html = b(urllib.urlopen(url).read(),"lxml")
-            titles = html.findAll('a',{'class':'vip'})
-            prices = html.findAll('span',{'class':'bold'})
-            for (i,j) in zip(titles,prices):
-                title,price = i.text,j.text
-                print(title)
-                print(price)
-        except:
-            pass
+    with open(str(filename)+'.csv', 'w',encoding='utf-8') as csvfile:
+        fieldnames = ['Title', 'Price']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(1,r+1):
+            print("*******************Titles and Prices On Page: "+str(i)+"************************")
+            try:
+                url="http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw="+str(search_string)+"&_pgn="+str(i)+"&_skc=100&rt=nc"
+                html = b(urllib.urlopen(url).read(),"lxml")
+                titles = html.findAll('a',{'class':'vip'})
+                prices = html.findAll('span',{'class':'bold'})
+                for (i,j) in zip(titles,prices):
+                    title,price = i.text,j.text[1:]
+                    writer.writerow({'Title':title, 'Price':price})
+            except:
+                pass
+            
 def info():
     print("""
               E-BAY TITLE AND PRICES SCRAPER(V1.0) BY PRATHAP
@@ -42,6 +46,7 @@ info()
 try:
     name = input("Enter the Product Name: ")
     pages = int(input("Enter How many pages to be scraped ( less than 200): "))
+    filename = input("Please, Name Your Output CSV File(without .csv): ")
     enterProduct(name)
     totalPages(pages)
     startCrawl()
